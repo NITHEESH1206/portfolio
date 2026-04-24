152ac7beed8aaf05f832d92247979de641219530
Nitheesh Kumar <nitheeshkumar0191@gmail.com>
2026-04-24 23:31:34 +0530

    force update page.js with Contact import

--- app/page.js
+++ app/page.js
@@ -3,10 +3,14 @@ import About from "../components/About"
 import Projects from "../components/Projects"
 import Marquee from "../components/Marquee"
 import Skills from "../components/Skills"
+import ProcessBanner from "../components/ProcessBanner"
+import Contact from "../components/Contact"
+import Footer from "../components/Footer"
 
 export default function Page() {
   return (
     <main className="relative">
+      {/* force update*/}
       <Hero />
       <About />
       <Projects />
