c9460e14e1fbc60f909b07a08037faaac3ebca20
Nitheesh Kumar <nitheeshkumar0191@gmail.com>
2026-04-24 23:07:58 +0530

    fix: use relative imports

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Projects from "@/components/Projects";
-import ProcessBanner from "@/components/Marquee";
-import Skills from "@/components/Skills";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+import Hero from "../components/Hero"
+import About from "../components/About"
+import Projects from "../components/Projects"
+import Marquee from "../components/Marquee"
+import Skills from "../components/Skills"
 
 export default function Page() {
   return (
